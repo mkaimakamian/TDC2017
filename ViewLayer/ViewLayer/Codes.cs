5ee789766a0ae2dbb7dc6707de354469c460c587
Mkaimakamian <mkc@outlook.com.ar>
2017-04-28 21:13:04 -0300

    Agregado de códigos

--- ViewLayer/ViewLayer/Codes.cs
+++ ViewLayer/ViewLayer/Codes.cs
@@ -8,14 +8,113 @@ namespace ViewLayer
 {
     public class Codes
     {
-        public const string GE001 = "GE001"; //herramientas
+        public const string GE001 = "GE001"; //Herramientas
+        public const string GE002 = "GE002"; //Seguridad
+        public const string GE003 = "GE003"; //Perfiles
+        public const string GE004 = "GE004"; //Depósito
+        public const string GE005 = "GE005"; //Centro de recepción
+        public const string GE006 = "GE006"; //Colaborador
+        public const string GE007 = "GE007"; //Grupos de trabajo
+        public const string GE008 = "GE008"; //Vehículo
+        public const string GE009 = "GE009"; //Destinatarios
+        public const string GE010 = "GE010"; //Tipo de ítem
+        public const string GE011 = "GE011"; //Stock
+        public const string GE012 = "GE012"; //Donadores
+        public const string GE013 = "GE013"; //Donaciones
+        public const string GE014 = "GE014"; //Hojas de ruta
+        public const string GE015 = "GE015"; //Retiros
+        public const string GE016 = "GE016"; //Órdenes de salida
+
         public const string OP001 = "OP001"; //Cambiar idioma
+        public const string OP002 = "OP002";
+        public const string OP003 = "OP003";
+        public const string OP004 = "OP004";
+        public const string OP005 = "OP005";
+        public const string OP006 = "OP006";
+        public const string OP007 = "OP007";
+        public const string OP008 = "OP008";
+        public const string OP009 = "OP009";
+        public const string OP0010 = "OP0010";
+        public const string OP011 = "OP011";
+        public const string OP012 = "OP012";
+        public const string OP013 = "OP013";
+        public const string OP014 = "OP014";
+        public const string OP015 = "OP015";
+        public const string OP016 = "OP016";
+        public const string OP017 = "OP017";
+        public const string OP018 = "OP018";
+        public const string OP019 = "OP019";
+        public const string OP020 = "OP020";
+        public const string OP021 = "OP021";
+        public const string OP022 = "OP022";
+        public const string OP023 = "OP023";
+        public const string OP024 = "OP024";
+        public const string OP025 = "OP025";
+        public const string OP026 = "OP026";
+        public const string OP027 = "OP027";
+        public const string OP028 = "OP028";
+        public const string OP029 = "OP029";
+        public const string OP030 = "OP030";
+        public const string OP031 = "OP031";
+        public const string OP032 = "OP032";
+        public const string OP033 = "OP033";
+        public const string OP034 = "OP034";
+        public const string OP035 = "OP035";
+        public const string OP036 = "OP036";
+        public const string OP037 = "OP037";
+        public const string OP038 = "OP038";
+        public const string OP039 = "OP039";
+        public const string OP040 = "OP040";
+        public const string OP041 = "OP041";
+        public const string OP042 = "OP042";
+        public const string OP043 = "OP043";
+        public const string OP044 = "OP044";
+        public const string OP045 = "OP045";
+        public const string OP046 = "OP046";
+        public const string OP047 = "OP047";
+        public const string OP048 = "OP048";
+        public const string OP049 = "OP049";
+        public const string OP050 = "OP050";
+        public const string OP051 = "OP051";
+        public const string OP052 = "OP052";
+        public const string OP053 = "OP053";
+        public const string OP054 = "OP054";
+        public const string OP055 = "OP055";
+        public const string OP056 = "OP056";
+        public const string OP057 = "OP057";
+        public const string OP058 = "OP058";
+        public const string OP059 = "OP059";
+        public const string OP060 = "OP060";
+        public const string OP061 = "OP061";
+        public const string OP062 = "OP062";
+        public const string OP063 = "OP063";
+        public const string OP064 = "OP064";
+        public const string OP065 = "OP065";
+        public const string OP066 = "OP066";
+        public const string OP067 = "OP067";
 
 
         //:::TRADUCCIONES:::
         public const string BTN_ACCEPT = "BTN_ACCEPT"; //Botçon Aceptar
         public const string BTN_CANCEL = "BTN_CANCEL"; //Botón Cancelar
         public const string MNU_GE001 = "MNU_GE001"; //Menú herramientas
+        public const string MNU_GE002 = "MNU_GE002"; //Seguridad
+        public const string MNU_GE003 = "MNU_GE003"; //Perfiles
+        public const string MNU_GE004 = "MNU_GE004"; //Depósito
+        public const string MNU_GE005 = "MNU_GE005"; //Centro de recepción
+        public const string MNU_GE006 = "MNU_GE006"; //Colaborador
+        public const string MNU_GE007 = "MNU_GE007"; //Grupos de trabajo
+        public const string MNU_GE008 = "MNU_GE008"; //Vehículo
+        public const string MNU_GE009 = "MNU_GE009"; //Destinatarios
+        public const string MNU_GE010 = "MNU_GE010"; //Tipo de ítem
+        public const string MNU_GE011 = "MNU_GE011"; //Stock
+        public const string MNU_GE012 = "MNU_GE012"; //Donadores
+        public const string MNU_GE013 = "MNU_GE013"; //Donaciones
+        public const string MNU_GE014 = "MNU_GE014"; //Hojas de ruta
+        public const string MNU_GE015 = "MNU_GE015"; //Retiros
+        public const string MNU_GE016 = "MNU_GE016"; //Órdenes de salida
+        
+                
         public const string MNU_OP001 = "MNU_OP001"; //Cambiar idioma
         public const string MNU_OP001_LBL_LANGUAGE = "MNU_OP001_LBL_LANGUAGE"; //Label de cambiar idioma
     }
