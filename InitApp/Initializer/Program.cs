59bf904a3b0c56fad6de69bcbd3fe147c26537d1
Mariano Kaimakamian Carrau <mkc@outlook.com.ar>
2017-07-06 23:38:00 -0300

    Agregado de script creador de la base de datos inicial.

---
+++ InitApp/Initializer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace Initializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string database = "CAMPOII";
+            string connStr = "Data Source=.\\UAI_EXPRESS; Initial Catalog=master; Integrated Security=True";
+
+            string name = Directory.GetCurrentDirectory() + "\\rhoddion_201726062126.bkp";
+            string sql = "";
+            sql = "CREATE DATABASE " + database + "  ; USE MASTER ALTER DATABASE " + database + " SET SINGLE_USER WITH ROLLBACK IMMEDIATE RESTORE DATABASE " + database + " FROM DISK = '" + name + "' WITH REPLACE";
+
+
+            SqlConnection connection = new SqlConnection(connStr);
+            SqlCommand command = new SqlCommand(sql, connection);
+            int result = -1;
+
+            connection.Open();
+            object row = command.ExecuteScalar();
+
+            if (row != null)
+            {
+                result = int.Parse(row.ToString());
+            }
+            connection.Close();
+
+        }
+
+    }
+}
